19da0318ddce4aa101192507f3cfe80e0023a854
Ashwani Kumar Arya <ashwaniarya2009@gmail.com>
2026-09-02 22:26:26 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (22.97%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,23 +1,24 @@
 class Solution {
     public int rob(int[] nums) {
-        if(nums.length == 1) return nums[0];
-        int[] dp = new int[nums.length];
-        Arrays.fill(dp,-1);
-        int first = util(nums, nums.length-1,1,dp);
+        if (nums.length == 1) return nums[0];
 
-        Arrays.fill(dp,-1);
-        int last = util(nums,nums.length-2,0,dp);
+        int first = util(nums,0,nums.length-2);
+        int last = util(nums,1,nums.length-1);
 
-        return Math.max(first, last);
+        return Math.max(first,last);
+
     }
 
-    public int util(int[] nums, int i, int end, int[] dp){
-        if(i<end) return 0;
+    public int util(int[] nums, int i, int end){
+        int prev2 = 0;
+        int prev1 = 0;
 
-        if(dp[i]!=-1) return dp[i];
+        for(int j = i; j<=end;j++){
+            int curr = Math.max(prev1,prev2 + nums[j]);
 
-        int skip = util(nums,i-1,end,dp);
-        int rob = util(nums,i-2,end,dp) + nums[i];
-        return dp[i] = Math.max(skip,rob);
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return prev1;
     }
 }

51821da524b8f283f538c9fa3886c505753e7a5b
Ashwani Kumar Arya <ashwaniarya2009@gmail.com>
2026-09-14 14:18:48 +0530

    Time: 9 ms (14.28%) | Memory: 47.3 MB (31.12%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.java
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.java
@@ -4,23 +4,36 @@ class Solution {
         int m = matrix.length;
         int[][] dp = new int[m][m];
         for(int i = 0; i <m;i++){
-            Arrays.fill(dp[i],Integer.MAX_VALUE);
+            dp[m-1][i] = matrix[m-1][i];
         }
-        for(int i = 0 ; i<matrix.length;i++){
-            min = Math.min(min,f(matrix, 0,i,dp));
+        for(int i = m-2 ; i>=0;i--){
+            for(int j = m-1; j>=0;j--){
+                int left = Integer.MAX_VALUE;
+                int right = Integer.MAX_VALUE;
+
+                if(j-1>=0) left = matrix[i][j] + dp[i+1][j-1];
+                if(j+1<m) right = matrix[i][j] + dp[i+1][j+1];
+                int down = matrix[i][j] + dp[i+1][j];
+
+                dp[i][j] = Math.min(down, Math.min(right, left));
+            }
+        }
+
+        for(int i = 0; i <m;i++){
+            min = Math.min(min,dp[0][i]);
         }
         return min;
     }
 
-    int f(int[][] matrix, int i , int j, int[][] dp){
-        if(i == matrix.length-1) return matrix[i][j];
-        if(dp[i][j] != Integer.MAX_VALUE) return dp[i][j];
-        int right = Integer.MAX_VALUE;
-        int left = Integer.MAX_VALUE;
-        if(j+1 <matrix.length) right = matrix[i][j] + f(matrix,i+1,j+1,dp);
-        if(j-1 >=0) left = matrix[i][j] + f(matrix,i+1,j-1,dp);
-        int down = matrix[i][j] + f(matrix,i+1,j,dp);
+    // int f(int[][] matrix, int i , int j, int[][] dp){
+    //     if(i == matrix.length-1) return matrix[i][j];
+    //     if(dp[i][j] != Integer.MAX_VALUE) return dp[i][j];
+    //     int right = Integer.MAX_VALUE;
+    //     int left = Integer.MAX_VALUE;
+    //     if(j+1 <matrix.length) right = matrix[i][j] + f(matrix,i+1,j+1,dp);
+    //     if(j-1 >=0) left = matrix[i][j] + f(matrix,i+1,j-1,dp);
+    //     int down = matrix[i][j] + f(matrix,i+1,j,dp);
 
-        return dp[i][j] = Math.min(right, Math.min(down, left));
-    }
+    //     return dp[i][j] = Math.min(right, Math.min(down, left));
+    // }
 }
